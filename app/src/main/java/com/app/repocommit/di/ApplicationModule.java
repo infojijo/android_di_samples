acfb1f03ab245e4f4b0af3e3da8eaee875e4896b
Jijo Jose <mail.cjnet@gmail.com>
2019-11-13 16:28:56 -0500

    More modules to ApplicationComponent.

--- app/src/main/java/com/app/repocommit/di/ApplicationModule.java
+++ app/src/main/java/com/app/repocommit/di/ApplicationModule.java
@@ -8,7 +8,7 @@ public class ApplicationModule {
 
     @Provides
     static String getAppName(){
+        //return type should be unique here for all the methods.
         return "Android DI App";
-
     }
 }
