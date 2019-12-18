d562056be8948f59baf6729023906d726e39d1f5
Jijo Jose <info.jijo@gmail.com>
2019-12-17 22:19:50 -0600

    Added Jetpack Navigation added first level

--- app/src/main/java/com/app/repocommit/ui/main/MainActivity.java
+++ app/src/main/java/com/app/repocommit/ui/main/MainActivity.java
@@ -4,42 +4,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.app.repocommit.BaseActivity;
 import com.app.repocommit.R;
-import com.app.repocommit.ui.main.post.PostFragment;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = "MainActivity";
+
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
-
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-        switch (menuItem.getItemId()) {
-            case R.id.nav_profile: {
-
-            }
-            break;
-            case R.id.nav_posts: {
-
-            }
-            break;
-        }
-        menuItem.setChecked(true); //to highlight the selected one
-        drawerLayout.closeDrawer(GravityCompat.START); // to close the drawer to the START of the Screen
-        return true;// click is consumed.
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,16 +32,40 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
 
-        Toast.makeText(this, "Logged Into Main Activity", Toast.LENGTH_SHORT).show();
-        TestFragment();
+        init();
     }
 
-    //for initiating the Profile Fragment.
-    private void TestFragment() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_container, new PostFragment())
-                .commit();
+    private void init(){
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch(menuItem.getItemId()){
+
+            case R.id.nav_profile:{
+
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.profileScreen);
+
+                break;
+            }
+
+            case R.id.nav_posts:{
+
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.postsScreen);
+
+                break;
+            }
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     @Override
@@ -68,10 +76,11 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()) {
-            case R.id.logout: {
+        switch (item.getItemId()){
+
+            case R.id.logout:{
                 sessionManager.logout();
                 return true;
             }
@@ -80,3 +89,4 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         return super.onOptionsItemSelected(item);
     }
 }
+
